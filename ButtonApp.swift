d146c5349c6b3b51a94c5a5d8b8912a95460662c
OLeg <146450111+grpolozun@users.noreply.github.com>
2023-09-29 16:25:00 +0300

    buttons

---
+++ ButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonApp.swift
+//  Button
+//
+//  Created by Oleg Savelyev on 21.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct ButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
